b1aca4ce31f702b4dd88157ef5feca976b590152
Nikica Trajkovski <trajkovs@usc.edu>
2017-02-27 17:20:08 -0800

    added more routes

--- Backend/src/server.php
+++ Backend/src/server.php
@@ -5,42 +5,35 @@ require_once('../vendor/autoload.php');
 require ('ProcessData.php');
 
 if ( isset($_GET["artistName"]) ) {
+  // Return a list of valid artists
   $dataProessor = new ProcessData();
   $_SESSION['dataProessor'] = $dataProessor;
-  print_r($_GET["artistName"]. "\n"); // take this out
   $result = $_SESSION['dataProessor']->searchArtist($_GET["artistName"]);
   $_SESSION['dataProessor'] = serialize($dataProessor);
-//  print_r(serialize($result));
-//echo "\n\n\n";
-  echo (serialize($result));
-
+  echo serialize($result);
 }
 else if ( isset($_GET["artistID"]) ) {
   // Return word map for artist
-  print_r($_GET["artistID"]. "\n");
-  $wordmap = unserialize($_SESSION['dataProessor']);
-
-//  $dp = new ProcessData();
-//  $result = $dp->searchArtist('Kendrick Lamar');
-  $result = $wordmap->generateCloud(/*$_GET['artistID']*/0);
-
-//  echo "DONE\n";
-
-//  echo "DONE AGAIN\n";
-  $_SESSION['dataProessor'] = serialize($wordmap);
-//  echo json_encode((array)$wordmap);
+  $processor = unserialize($_SESSION['dataProessor']);
+  $result = $processor->generateCloud($_GET['artistID']);
+  $_SESSION['dataProessor'] = serialize($processor);
+  echo serialize($result);
 }
 else if ( isset($_GET["word"]) ) {
-  // return songs that match a word
-  print_r($_GET["word"]. "\n");
-  $songList = $dataProessor->getSongs($_GET["word"]);
-  echo json_encode($songList);
+  // Return songs that match a word
+  $processor = unserialize($_SESSION['dataProessor']);
+  $songList = $processor->getSongs($_GET["word"]);
+  $_SESSION['dataProessor'] = serialize($processor);
+  echo serialize($songList);
 }
-//else if ( $_GET["songTitle"] && $_GET["artistName"] ) {
-//  $lyircs = $dataProessor->getLyrics($_GET["songTitle"], $_GET["artistName"]);
-//  echo json_encode($lyircs);
-//}
+else if ( $_GET["songTitle"] && $_GET["artistName"] ) {
+  // Return lyric link for a song
+  $processor = unserialize($_SESSION['dataProessor']);
+  $lyircs = $processor->getLyrics($_GET["songTitle"], $_GET["artistName"]);
+  $_SESSION['dataProessor'] = serialize($processor);
+  echo serialize($lyircs);
+}
 else {
-  echo "lyricsBorne API\n";
+  echo "lyricsBorne API\nInvalid query\n";
 }
 ?>
